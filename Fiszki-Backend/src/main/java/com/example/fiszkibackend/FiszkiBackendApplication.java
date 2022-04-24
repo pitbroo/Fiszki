b9636fd233df0ecbf6c14624ddced2ba15dcfd12
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2022-04-24 23:25:50 +0200

    initial commit

---
+++ Fiszki-Backend/src/main/java/com/example/fiszkibackend/FiszkiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiszkibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiszkiBackendApplication.class, args);
+    }
+
+}
---
+++ Fiszki-Backend/src/test/java/com/example/fiszkibackend/FiszkiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiszkibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiszkiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
